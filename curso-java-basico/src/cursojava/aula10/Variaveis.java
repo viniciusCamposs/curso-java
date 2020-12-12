4729119bec6d2122a222ff1ed144dcc1b9088eff
Vinicius Alves <vinicius.campos5@fatec.sp.gov.br>
2020-12-12 18:08:55 -0300

    Aula 10 e 11

---
+++ curso-java-basico/src/cursojava/aula10/Variaveis.java
@@ -0,0 +1,20 @@
+package cursojava.aula10;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		
+		int idade = 19;
+		String nome = "Vinicius";
+		String nomeDoMeuCachorro = "Billy";
+		
+		
+		idade = 25;
+		
+		System.out.println("Idade: " + idade
+						  +"\nNome: " + nome
+						  +"\nCachorro: " + nomeDoMeuCachorro);
+
+	}
+
+}
---
+++ curso-java-basico/src/cursojava/aula11/CuriosidadesInt.java
@@ -0,0 +1,16 @@
+package cursojava.aula11;
+
+public class CuriosidadesInt {
+
+	public static void main(String[] args) {
+		
+		
+		int var1 = 2147483647;
+		
+		int var2 = 100;
+		
+		System.out.println(var1 + var2);
+
+	}
+
+}
