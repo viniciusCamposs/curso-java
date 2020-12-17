00ee341ce199da841fc53e968ced9ce73422492e
Vinicius Alves <vinicius.campos5@fatec.sp.gov.br>
2020-12-17 20:27:04 -0300

    Aula 17

---
+++ curso-java-basico/src/cursojava/aula17/LoopFor.java
@@ -0,0 +1,34 @@
+package cursojava.aula17;
+
+import java.util.Scanner;
+
+public class LoopFor {
+
+	public static void main(String[] args) {
+		/*
+		for(int i=0; i<10; i++) {
+			System.out.println("Valor de I: " + (i+1));
+		}
+		*/
+		
+		/*
+		for(int i=0, j=10; i<j; i++, j--) {
+			System.out.println("Valor de I e J: " + i + " " + j);
+		}
+		*/
+		/*
+		int i=0;
+		for(; i < 5;) {
+			System.out.println("Valor de i: " + (i+1));
+			i++;
+		}
+		*/
+		int soma = 0;
+		
+		for(int i=1; i<5; soma+= i++);
+		
+		System.out.println("O valor da soma é: " + soma);
+
+	}
+
+}
