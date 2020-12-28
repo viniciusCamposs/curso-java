1896e3dbb398b13c3f406e1f652a912979dbc22d
Vinicius Alves <vinicius.campos5@fatec.sp.gov.br>
2020-12-27 21:27:56 -0300

    Aula 24

---
+++ curso-java-basico/src/cursojava/aula24/TesteCarro.java
@@ -0,0 +1,18 @@
+package cursojava.aula24;
+
+public class TesteCarro {
+
+	public static void main(String[] args) {
+		
+		Carro van = new Carro();
+		van.marca = "Fiat";
+		van.modelo = "Ducato";
+		van.numPassageiros = 10;
+		van.capCombustivel = 100;
+		van.capCombustivel = 0.2;
+		
+		System.out.println(van.marca);
+
+	}
+
+}
