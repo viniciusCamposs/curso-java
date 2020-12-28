25e10fca5dbf72233e81dd49a333863499cbc639
Vinicius Alves <vinicius.campos5@fatec.sp.gov.br>
2020-12-28 20:07:55 -0300

    Aula 33

---
+++ curso-java-basico/src/cursojava/aula33/TesteCalculadora.java
@@ -0,0 +1,13 @@
+package cursojava.aula33;
+
+public class TesteCalculadora {
+
+	public static void main(String[] args) {
+		
+		MinhaCalculadora calc = new MinhaCalculadora();
+		
+		System.out.println(calc.soma(1, 2));
+
+	}
+
+}
