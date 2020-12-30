5b5bc69fac975e12e3c0c1725a3ac9d6b1669b53
Vinicius <vinicius.campos@fatec.sp.gov.br>
2020-12-30 11:22:03 -0300

    Aula 38

--- curso-java-basico/src/cursojava/aula37/Teste.java
+++ curso-java-basico/src/cursojava/aula37/Teste.java
@@ -3,7 +3,8 @@ package cursojava.aula37;
 public class Teste {
 
 	public static void main(String[] args) {
-		Aluno aluno = new Aluno();
+		
+		/*Aluno aluno = new Aluno();
 
 		Professor professor = new Professor();
 
@@ -13,6 +14,12 @@ public class Teste {
 		System.out.println(aluno.getNome());
 		
 		professor.setTelefoneCelular("(19) 99999-9999");
+		*/
+		
+		Pessoa aluno = new Aluno();
+		
+		Pessoa professor = new Professor();
+		
 	}
 
 }
---
+++ curso-java-basico/src/cursojava/aula38/Teste.java
@@ -0,0 +1,11 @@
+package cursojava.aula38;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
