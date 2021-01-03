ecfc19674de78c1ccfb8c6b64988a9f1280cdf8e
Vinicius Alves <vinicius.campos5@fatec.sp.gov.br>
2021-01-03 17:25:37 -0300

    Aula 50

---
+++ curso-java-basico/src/cursojava/aula50/Usandothrows.java
@@ -0,0 +1,25 @@
+package cursojava.aula50;
+
+import java.util.Scanner;
+
+public class Usandothrows {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Entre com um número decimal: ");
+		try {
+			double num = leNumero();
+		} catch (Exception e) {
+			System.out.println("Entrada inválida");
+			e.printStackTrace();
+		}
+
+	}
+	
+	public static double leNumero() throws Exception{
+		Scanner scan = new Scanner(System.in);
+		double num = scan.nextDouble();
+		return num;
+	}
+
+}
