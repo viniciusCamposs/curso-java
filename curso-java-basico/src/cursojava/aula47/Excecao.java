bc271c412502030943c4a6836a3f54a87506295f
Vinicius Alves <vinicius.campos5@fatec.sp.gov.br>
2021-01-03 13:43:46 -0300

    Aula 48

--- curso-java-basico/src/cursojava/aula47/Excecao.java
+++ curso-java-basico/src/cursojava/aula47/Excecao.java
@@ -3,17 +3,17 @@ package cursojava.aula47;
 public class Excecao {
 	public static void main(String[] args) {
 		try {
-		int[] vetor = new int[4];
-		
-		System.out.println("Antes da exception");
-		
-		vetor[4] = 2;
-		
-		System.out.println("Esse texto não será impresso");
-		}catch(ArrayIndexOutOfBoundsException exception) {
+			int[] vetor = new int[4];
+
+			System.out.println("Antes da exception");
+
+			vetor[4] = 2;
+
+			System.out.println("Esse texto não será impresso");
+		} catch (ArrayIndexOutOfBoundsException exception) {
 			System.out.println("Exceção ao acessar um índice do vetor que não existe");
 		}
 		System.out.println("Esse texto será impresso após a exception");
-		
+
 	}
 }
