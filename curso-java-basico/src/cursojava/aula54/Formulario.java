71b7cb19653c8e6a91e0b34617c977790099da6a
Vinicius <vinicius.campos@fatec.sp.gov.br>
2021-01-04 10:13:49 -0300

    Aula 54

---
+++ curso-java-basico/src/cursojava/aula54/Formulario.java
@@ -0,0 +1,19 @@
+package cursojava.aula54;
+
+public class Formulario {
+	
+	enum Genero {
+		FEMININO('F'), MASCULINO('M');
+		
+		private char valor;
+		
+		Genero(char valor){
+			this.valor = valor;
+		}
+	}
+	
+	private String nome;
+	private Genero genero;
+	
+	
+}
---
+++ curso-java-basico/src/cursojava/aula54/TesteEnum.java
@@ -0,0 +1,12 @@
+package cursojava.aula54;
+
+public class TesteEnum {
+	public static void main(String[] args) {
+		DiaSemana dia = DiaSemana.SEGUNDA;
+		
+		System.out.println(dia.toString() + " - " + dia.getValor());
+		
+		Data data = new Data(1, 4, 2016, DiaSemana.SEXTA);
+		System.out.println(data.getAno());
+	}
+}
