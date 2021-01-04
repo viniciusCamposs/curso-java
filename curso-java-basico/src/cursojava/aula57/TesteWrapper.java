0f1492b6d07ee8a2d5a781d8a66a3b874740c583
Vinicius <vinicius.campos@fatec.sp.gov.br>
2021-01-04 11:05:22 -0300

    Aula 57

---
+++ curso-java-basico/src/cursojava/aula57/TesteWrapper.java
@@ -0,0 +1,43 @@
+package cursojava.aula57;
+
+public class TesteWrapper {
+
+	public static void main(String[] args) {
+		
+		short num1 = 1;
+		byte num2 = 10;
+		int num3 = 100;
+		float num5 = 3.5f;
+		double num6 = 3.5;
+		boolean flag = true;
+		char a = 'a';
+		
+		Short num7 = new Short((short) 1);
+		Byte num8 = new Byte((byte) 10);
+		Integer num9 = new Integer(100);
+		Long num10 = new Long(10000l);
+		Float num11 = new Float(3.5f);
+		Double num12 = new Double(3.5555);
+		Boolean flag2 = new Boolean(true);
+		Character b = new Character('b');
+		
+		Integer num13 = new Integer("100");
+		Double num14 = new Double("3.5");
+		
+		System.out.println(num13.intValue());
+		System.out.println(num13.longValue());
+		
+		Long num15 = num13.longValue();
+		
+		int num16 = Integer.parseInt("10000");
+		
+		double num17 = Double.parseDouble("3.555");
+		System.out.println(num17);
+		
+		Integer num18 = Integer.valueOf(1234);
+		System.out.println(num18);
+		
+		System.out.println(num9.equals(num13));
+	}
+
+}
