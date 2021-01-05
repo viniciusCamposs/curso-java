adfee0ffb7de8e6a601f04b32fd7da9e2afa325d
Vinicius Alves <vinicius.campos5@fatec.sp.gov.br>
2021-01-04 21:07:23 -0300

    Aula 60

---
+++ curso-java-basico/src/cursojava/aula60/EscopoVariaveis.java
@@ -0,0 +1,34 @@
+package cursojava.aula60;
+
+public class EscopoVariaveis {
+	private int valor;
+
+	public int getValor() {
+		return valor;
+	}
+
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
+
+	public int calculaValor(int valor) {
+		valor = valor + 10;
+
+		return valor;
+	}
+	
+	public int teste() {
+		int valor = 5;
+		
+		if(true) {
+			valor--;
+		}
+		
+		return valor;
+	}
+	
+	public void outroTeste() {
+		
+	}
+
+}
