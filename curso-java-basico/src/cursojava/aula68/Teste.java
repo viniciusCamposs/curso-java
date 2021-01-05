a2c0866928fc8ac17968826f5455f9cd8062b3e0
Vinicius <vinicius.campos@fatec.sp.gov.br>
2021-01-05 11:02:26 -0300

    Aula 68

---
+++ curso-java-basico/src/cursojava/aula68/Teste.java
@@ -0,0 +1,13 @@
+package cursojava.aula68;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		MinhaThreadRunnable thread01 = new MinhaThreadRunnable("#1", 500);
+		MinhaThreadRunnable thread02 = new MinhaThreadRunnable("#1", 1100);
+		MinhaThreadRunnable thread03 = new MinhaThreadRunnable("#1", 1200);
+
+
+	}
+
+}
