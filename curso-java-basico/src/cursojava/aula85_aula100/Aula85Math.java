57c1c027558a4aefeb7a8fbfd5fded4f248dea03
Vinicius <vinicius.campos@fatec.sp.gov.br>
2021-01-06 11:08:01 -0300

    Aula 85

---
+++ curso-java-basico/src/cursojava/aula85_aula100/Aula85Math.java
@@ -0,0 +1,33 @@
+package cursojava.aula85_aula100;
+
+public class Aula85Math {
+
+	public static void main(String[] args) {
+
+		System.out.println(Math.pow(2, 3));
+
+		System.out.println(Math.round(4.7));
+
+		System.out.println(Math.round(4.4));
+
+		System.out.println(Math.ceil(4.4));
+
+		System.out.println(Math.ceil(4.7));
+
+		System.out.println(Math.floor(4.4));
+
+		System.out.println(Math.floor(4.7));
+
+		System.out.println(Math.round(Math.random()));
+		
+		System.out.println(Math.sin(Math.toRadians(30)));
+		
+		System.out.println(Math.cos(Math.toRadians(1)));
+		
+		System.out.println(Math.tan(Math.toRadians(45)));
+		
+		
+
+	}
+
+}
