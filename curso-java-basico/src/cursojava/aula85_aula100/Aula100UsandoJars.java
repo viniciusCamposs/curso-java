bbc91accb2a63c330713ebd04a9d7cb5de2bf0b4
Vinicius <vinicius.campos@fatec.sp.gov.br>
2021-01-07 12:02:30 -0300

    Aula 100

---
+++ curso-java-basico/src/cursojava/aula85_aula100/Aula100UsandoJars.java
@@ -0,0 +1,7 @@
+package cursojava.aula85_aula100;
+
+public class Aula100UsandoJars {
+	public static void main(String[] args) {
+
+	}
+}
--- curso-java-basico/src/cursojava/aula85_aula100/Aula99NumerosAleatorios.java
+++ curso-java-basico/src/cursojava/aula85_aula100/Aula99NumerosAleatorios.java
@@ -5,11 +5,11 @@ import java.util.Random;
 public class Aula99NumerosAleatorios {
 
 	public static void main(String[] args) {
-		
-		//System.out.println(Math.floor((Math.random())*10));
-		
+
+		// System.out.println(Math.floor((Math.random())*10));
+
 		Random numeros = new Random();
-		
+
 		System.out.println(numeros.nextInt(5 + 1));
 
 	}
