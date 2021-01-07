6e1902faf2214a239ef6b207bae07efc48ed8d74
Vinicius <vinicius.campos@fatec.sp.gov.br>
2021-01-07 11:03:36 -0300

    Aula 97

---
+++ curso-java-basico/src/cursojava/aula85_aula100/Aula97DecimalFormat.java
@@ -0,0 +1,28 @@
+package cursojava.aula85_aula100;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+
+public class Aula97DecimalFormat {
+	public static void main(String[] args) {
+		
+		String padrao = "###,###.##";
+		DecimalFormat df = new DecimalFormat(padrao);
+		
+		System.out.println(df.format(123467890.123));
+		
+		DecimalFormatSymbols dfs = new DecimalFormatSymbols(new Locale("pt", "Brazil"));
+		dfs.setDecimalSeparator(',');
+		dfs.setGroupingSeparator('.');
+		
+		String padrao2 = "###,###.##";
+		df = new DecimalFormat(padrao2, dfs);
+		df.setGroupingSize(3);
+		System.out.println(df.format(123467890.123));
+		
+		
+		
+		
+	}
+}
