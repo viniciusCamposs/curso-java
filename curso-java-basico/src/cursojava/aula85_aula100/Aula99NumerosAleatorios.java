5f6d5cdd3d55e0b904dac36517e1bcca817d05fd
Vinicius <vinicius.campos@fatec.sp.gov.br>
2021-01-07 11:46:38 -0300

    Aula 99

---
+++ curso-java-basico/src/cursojava/aula85_aula100/Aula99NumerosAleatorios.java
@@ -0,0 +1,17 @@
+package cursojava.aula85_aula100;
+
+import java.util.Random;
+
+public class Aula99NumerosAleatorios {
+
+	public static void main(String[] args) {
+		
+		//System.out.println(Math.floor((Math.random())*10));
+		
+		Random numeros = new Random();
+		
+		System.out.println(numeros.nextInt(5 + 1));
+
+	}
+
+}
